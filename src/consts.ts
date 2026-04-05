85279f84aa80c4f1775e38cd060218fec00f486a
geraldfoo <37094368+geraldfoo@users.noreply.github.com>
2026-04-05 12:19:41 -0400

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Ripeful Trading";
+export const SITE_DESCRIPTION = "Ripeful Trading brings the depth and intensity of Southeast Asia’s most iconic flavors into a new world of modern desserts—where bold meets elegant, and curiosity becomes indulgence.";
